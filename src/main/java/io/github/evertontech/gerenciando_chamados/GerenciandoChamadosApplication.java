74b58a90c457cb9e812320c16197c40a09d3fec8
Everton Santos <everton_projeto@outlook.com>
2024-08-02 15:28:08 -0300

    formatando arquivos

--- src/main/java/io/github/evertontech/gerenciando_chamados/GerenciandoChamadosApplication.java
+++ src/main/java/io/github/evertontech/gerenciando_chamados/GerenciandoChamadosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerenciandoChamadosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciandoChamadosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciandoChamadosApplication.class, args);
+    }
 
 }
