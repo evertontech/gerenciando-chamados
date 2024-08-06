5aedc0fa71324cfeaa0491d8d3194ff8dfcc8e10
Everton Santos <everton_projeto@outlook.com>
2024-08-06 15:23:51 -0300

    metodo post de categorias

--- src/main/java/io/github/evertontech/gerenciando_chamados/controller/CategoriaChamadoController.java
+++ src/main/java/io/github/evertontech/gerenciando_chamados/controller/CategoriaChamadoController.java
@@ -1,4 +1,19 @@
 package io.github.evertontech.gerenciando_chamados.controller;
 
+import io.github.evertontech.gerenciando_chamados.model.entity.CategoriaChamado;
+import io.github.evertontech.gerenciando_chamados.model.repository.CategoriaChamadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/categorias-de-chamado")
 public class CategoriaChamadoController {
+
+    @Autowired
+    CategoriaChamadoRepository categoriaChamadoRepository;
+
+    @PostMapping
+    public CategoriaChamado criar(@RequestBody CategoriaChamado categoriaChamado) {
+        return categoriaChamadoRepository.save(categoriaChamado);
+    }
 }
