07f7a3e34446d075993e728781eba8ef8d565e60
Everton Santos <everton_projeto@outlook.com>
2024-08-07 14:25:40 -0300

    atualizacao das controllers

--- src/main/java/io/github/evertontech/gerenciando_chamados/controller/ChamadoController.java
+++ src/main/java/io/github/evertontech/gerenciando_chamados/controller/ChamadoController.java
@@ -1,4 +1,10 @@
 package io.github.evertontech.gerenciando_chamados.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/chamados")
 public class ChamadoController {
+
 }
--- src/main/java/io/github/evertontech/gerenciando_chamados/controller/TecnicoController.java
+++ src/main/java/io/github/evertontech/gerenciando_chamados/controller/TecnicoController.java
@@ -1,4 +1,42 @@
 package io.github.evertontech.gerenciando_chamados.controller;
 
+import io.github.evertontech.gerenciando_chamados.model.entity.Tecnico;
+import io.github.evertontech.gerenciando_chamados.model.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/tecnicos")
 public class TecnicoController {
+
+    @Autowired
+    TecnicoRepository tecnicoRepository;
+
+    @PostMapping
+    public Tecnico criar(@RequestBody Tecnico tecnico) {
+        return tecnicoRepository.save(tecnico);
+    }
+
+    @GetMapping
+    public Iterable<Tecnico> obterTodos() {
+        return tecnicoRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Tecnico> obterPorId(@PathVariable Long id) {
+        return tecnicoRepository.findById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Tecnico atualizar(@RequestBody Tecnico tecnico, @PathVariable Long id) {
+        tecnico.setId(id);
+        return tecnicoRepository.save(tecnico);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletar(@PathVariable Long id) {
+        tecnicoRepository.deleteById(id);
+    }
 }
