d91508d7fcb111e2a5c7dcc4ae68bff7868b2e44
Everton Santos <everton_projeto@outlook.com>
2024-08-07 14:24:59 -0300

    implementando controller categoria chamado

--- src/main/java/io/github/evertontech/gerenciando_chamados/controller/CategoriaChamadoController.java
+++ src/main/java/io/github/evertontech/gerenciando_chamados/controller/CategoriaChamadoController.java
@@ -5,6 +5,8 @@ import io.github.evertontech.gerenciando_chamados.model.repository.CategoriaCham
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(path = "/categorias-de-chamado")
 public class CategoriaChamadoController {
@@ -20,5 +22,21 @@ public class CategoriaChamadoController {
     @GetMapping
     public Iterable<CategoriaChamado> listar() {
         return categoriaChamadoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<CategoriaChamado> obterPorId(@PathVariable Long id) {
+        return categoriaChamadoRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public CategoriaChamado atualizar(@RequestBody CategoriaChamado categoriaChamado, @PathVariable Long id) {
+        categoriaChamado.setId(id);
+        return categoriaChamadoRepository.save(categoriaChamado);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletar(@PathVariable Long id) {
+        categoriaChamadoRepository.deleteById(id);
     }
 }
